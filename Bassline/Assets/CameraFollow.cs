2cc1efc8892fd470621fb90dcb247f187990dece
Kyle Lekkas <35651136+Kleonlekkas@users.noreply.github.com>
2018-03-07 14:23:46 -0500

    Added camera offset to the player follow

--- Bassline/Assets/CameraFollow.cs
+++ Bassline/Assets/CameraFollow.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +7,15 @@ public class CameraFollow : MonoBehaviour {
     public Transform Followplatform;
     public Transform Player;
 
+    int cameraOffset;
+
+    private void Start()
+    {
+        cameraOffset = 7;
+    }
     void Update()
     {
-        Followplatform.position = new Vector3(Player.position.x, Followplatform.position.y, Followplatform.position.z);
+        Followplatform.position = new Vector3((Player.position.x + cameraOffset), Followplatform.position.y, Followplatform.position.z);
     }
 
 }
