71338cbeb8fc60b87e8e3e60ac9a8cc0288b7fad
Ryan MacKenzie <rxm5568@g.rit.edu>
2018-03-10 06:47:47 -0500

    Minor Updates

--- Bassline/Assets/Scripts/ANoteScript.cs
+++ Bassline/Assets/Scripts/ANoteScript.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ANoteScript : MonoBehaviour {
-    public GameObject myPrefab;
-    public float maxScale;
-    public float scaleFactor;
+public class ANoteScript : MonoBehaviour
+{
+    public bool isActive; //bool to track if the bar is active
+    public string activeKey; //str containing the key that will expand the bar if active
+    public float maxScale; //maximum size the bar can scale to
+    public float scaleFactor; //how much the bar scales by per second
 
 	// Use this for initialization
 	void Start ()
@@ -17,35 +19,43 @@ public class ANoteScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-
-        if (Input.GetKey("space"))
+        if(isActive) //checks if bar is active
         {
-            transform.localScale += new Vector3(0, scaleFactor * Time.deltaTime, 0);
-            this.transform.position += new Vector3(0, scaleFactor / 2 * Time.deltaTime, 0);
-            if (this.transform.localScale.y > maxScale)
+            if (Input.GetKey(activeKey)) //checks if active key is depressed
             {
-                this.transform.localScale = new Vector3(1, maxScale, 1);
+                //increase scale and transform position to make it appear the bar is rising
+                transform.localScale += new Vector3(0, scaleFactor * Time.deltaTime, 0);
+                this.transform.position += new Vector3(0, scaleFactor / 2 * Time.deltaTime, 0);
+
+                //don't let it get too large
+                if (this.transform.localScale.y > maxScale)
+                {
+                    this.transform.localScale = new Vector3(1, maxScale, 1);
+                }
+                //or high
+                if (this.transform.position.y > (maxScale - 1) / 2)
+                {
+                    this.transform.position = new Vector3(this.transform.position.x, (maxScale - 1) / 2, this.transform.position.z);
+                }
             }
-            if (this.transform.position.y > (maxScale - 1) / 2)
+            else
             {
-                this.transform.position = new Vector3(this.transform.position.x, (maxScale - 1) / 2, this.transform.position.z);
+                transform.localScale += new Vector3(0, -scaleFactor * Time.deltaTime, 0);
+                this.transform.position += new Vector3(0, -scaleFactor / 2 * Time.deltaTime, 0);
+                if (this.transform.localScale.y < 1)
+                {
+                    this.transform.localScale = new Vector3(1, 1, 1);
+                }
+                if (this.transform.position.y < 0)
+                {
+                    this.transform.position = new Vector3(this.transform.position.x, 0, this.transform.position.z);
+                }
             }
         }
         else
         {
-            transform.localScale += new Vector3(0, -scaleFactor * Time.deltaTime, 0);
-            this.transform.position += new Vector3(0, -scaleFactor / 2 * Time.deltaTime, 0);
-            if (this.transform.localScale.y < 1)
-            {
-                this.transform.localScale = new Vector3(1, 1, 1);
-            }
-            if (this.transform.position.y < 0)
-            {
-                this.transform.position = new Vector3(this.transform.position.x, 0, this.transform.position.z);
-            }
+
         }
-
-
     }
 
 
