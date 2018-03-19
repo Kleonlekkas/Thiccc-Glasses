586361df9a53970491885984074b9efd3826d4be
Patrick Bell <pvb2147@g.rit.edu>
2018-03-19 19:45:23 -0400

    Ok, here are actual changes, guys

---
+++ Bassline/Assets/Scripts/StartButtonScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonScript : MonoBehaviour {
+	public void Load(int scenebase)
+	{
+		SceneManager.LoadScene (scenebase);
+	}
+}
