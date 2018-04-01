2c8284b1692a8b8942a2f83b42b9a02dd80fd2c3
TIMSTABLET\Tim <ascenciot123@gmail.com>
2018-04-01 19:37:14 -0400

    Collectibles auto generated along map

--- Bassline/Assets/Scripts/GameManager.cs
+++ Bassline/Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     List<GameObject> bars = new List<GameObject>();
     public GameObject barPrefab; // set up generation of bars
+    public GameObject collectPrefab; // set up generation of bars
 
     // Use this for initialization
     void Start ()
@@ -17,6 +18,15 @@ public class GameManager : MonoBehaviour
             bar.GetComponent<ANoteScript>().activeKeytwo = "s";
             bar.GetComponent<ANoteScript>().activeKeythree = "d";
             bars.Add(bar);
+        }
+
+        //Automatically Generate Collectible Prefabs
+        for (int i = 0; i < 100; i++)
+        {
+            GameObject collectible = Instantiate(collectPrefab, new Vector3(i * 210.0F, 30.0f, 0), Quaternion.identity);
+            GameObject collectible1 = Instantiate(collectPrefab, new Vector3(i * 225.0F, 40.0f, 0), Quaternion.identity);
+            GameObject collectible2 = Instantiate(collectPrefab, new Vector3((i * 230.0F)+10.0f, 25.0f, 0), Quaternion.identity);
+            GameObject collectible3 = Instantiate(collectPrefab, new Vector3((i * 243.0F)-10.0f, 50.0f, 0), Quaternion.identity);
         }
     }
 	
