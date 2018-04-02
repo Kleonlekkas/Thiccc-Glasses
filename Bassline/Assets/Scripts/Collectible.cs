58dde53257e1c593a61f34f92e1867f44c1b4719
TIMSTABLET\Tim <ascenciot123@gmail.com>
2018-04-01 20:15:19 -0400

    Scoring system implemented

--- Bassline/Assets/Scripts/Collectible.cs
+++ Bassline/Assets/Scripts/Collectible.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collectible : MonoBehaviour
 {
     private GameObject player;
 
+    private static int score;
+    public Text scoreText;
+
 	// Use this for initialization
 	void Start ()
     {
+        score = 0;
+
         player = GameObject.FindWithTag("Player");
     }
 	
@@ -17,6 +23,8 @@ public class Collectible : MonoBehaviour
     {
 		if((player.transform.position - this.transform.position).magnitude < 5)
         {
+            score += 100;
+            scoreText.text = "Score: " + score.ToString();
             Destroy(gameObject);
         }
 	}
