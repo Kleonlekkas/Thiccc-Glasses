f46a1b4eece03ccce054cde11d00ff4f9476bce3
TIMSTABLET\Tim <ascenciot123@gmail.com>
2018-04-14 14:01:34 -0400

    Create solution add movePlayer script and adjust assets.

---
+++ iCabbage/Assets/Script/movePlayer.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayer : MonoBehaviour {
+
+    public GameObject playerObj;
+
+    private bool isLeftBlocked;
+    private bool isRightBlocked;
+    private bool isTopBlocked;
+    private bool isBottomBlocked;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        isLeftBlocked = false;
+        isRightBlocked = false;
+        isTopBlocked = false;
+        isBottomBlocked = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (playerObj.transform.position.x < -12.0f) { isLeftBlocked = true; }
+        else if (playerObj.transform.position.x > 12.0f) { isRightBlocked = true; }
+        else if (playerObj.transform.position.z < -5.0f) { isBottomBlocked = true; }
+        else if (playerObj.transform.position.z > 5.0f) { isTopBlocked = true; }
+        else
+        {
+            isLeftBlocked = isRightBlocked = isBottomBlocked = isTopBlocked = false;
+        }
+
+        if (!isTopBlocked)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                playerObj.transform.position = new Vector3(
+                    playerObj.transform.position.x,
+                    playerObj.transform.position.y,
+                    playerObj.transform.position.z + 2.0f);
+            }
+        }
+        if (!isLeftBlocked) { 
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                playerObj.transform.position = new Vector3(
+                    playerObj.transform.position.x - 3.0f,
+                    playerObj.transform.position.y,
+                    playerObj.transform.position.z);
+            }
+        }
+        if (!isBottomBlocked)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                playerObj.transform.position = new Vector3(
+                    playerObj.transform.position.x,
+                    playerObj.transform.position.y,
+                    playerObj.transform.position.z - 2.0f);
+            }
+        }
+        if (!isRightBlocked)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                playerObj.transform.position = new Vector3(
+                    playerObj.transform.position.x + 3.0f,
+                    playerObj.transform.position.y,
+                    playerObj.transform.position.z);
+            }
+        }
+    }
+}
